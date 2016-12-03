f7813aa3cf13856d9169587b858fdd53ebd292fc
Hyeon Kim <simnalamburt@gmail.com>
2016-12-03 15:46:11 +0900

    Create README, add LICENSE files, make simple documentation

---
+++ README.md
@@ -0,0 +1,27 @@
+elm-mqtt
+========
+This package is client library for [MQTT] protocol. It is based on the [MQTT.js]
+project.
+
+<!-- TODO: Example should be in here
+
+```elm
+import Mqtt
+
+{- blabla -}
+```
+
+-->
+
+<br>
+
+--------
+
+`elm-mqtt` is primarily distributed under the terms of both the [MIT license] and
+the [Apache License (Version 2.0)]. See [COPYRIGHT] for details.
+
+[MQTT]: http://mqtt.org/
+[MQTT.js]: https://github.com/mqttjs/MQTT.js
+[MIT license]: LICENSE-MIT
+[Apache License (Version 2.0)]: LICENSE-APACHE
+[COPYRIGHT]: COPYRIGHT
--- src/Mqtt.elm
+++ src/Mqtt.elm
@@ -1,4 +1,23 @@
+{-
+Copyright 2016 Hyeon Kim
+
+Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+<LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+option. This file may not be copied, modified, or distributed
+except according to those terms.
+-}
+
 module Mqtt exposing (hello)
+{-| This package is client library for [MQTT] protocol. It is based on the
+[MQTT.js] project.
+
+# Placeholder
+@docs hello
+
+[MQTT]: http://mqtt.org/
+[MQTT.js]: https://github.com/mqttjs/MQTT.js
+-}
 
 {-| A placeholder function for early-development stage.
 

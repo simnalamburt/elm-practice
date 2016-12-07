a4df65e181d264cb9d24df22f8f4f1f8fc9e8829
Hyeon Kim <simnalamburt@gmail.com>
2016-12-07 21:47:23 +0900

    MyRandom 시작

---
+++ src/App.elm
@@ -0,0 +1,41 @@
+import Html exposing (..)
+import Html.Events exposing (..)
+import MyRandom
+
+main = Html.program
+  {
+    init = init,
+    view = view,
+    update = update,
+    subscriptions = subscriptions
+  }
+
+-- MODEL
+type alias Model = { dieFace : Int }
+
+init : (Model, Cmd Msg)
+init = (Model 1, Cmd.none)
+
+-- UPDATE
+type Msg = Roll
+         | NewFace Int
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    Roll ->
+      (model, MyRandom.generate NewFace (MyRandom.int 1 6))
+    NewFace newFace ->
+      (Model newFace, Cmd.none)
+
+-- SUBSCRIPTIONS
+subscriptions : Model -> Sub Msg
+subscriptions model = Sub.none
+
+-- VIEW
+view : Model -> Html Msg
+view model =
+  div [] [
+    h1 [] [ text (toString model.dieFace) ],
+    button [ onClick Roll ] [ text "Roll" ]
+  ]

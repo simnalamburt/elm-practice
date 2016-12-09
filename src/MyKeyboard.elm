04375b88d985d05bd07c0fd2cfb07773d8d05aba
Hyeon Kim <simnalamburt@gmail.com>
2016-12-09 11:51:14 +0900

    주석 강화

--- src/MyKeyboard.elm
+++ src/MyKeyboard.elm
@@ -1,4 +1,13 @@
-{- TODO: 저작권 명시 -}
+{-
+Copyright 2016 Hyeon Kim
+
+Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+<LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+option. This file may not be copied, modified, or distributed
+except according to those terms.
+-}
+
 effect module MyKeyboard where { subscription = MySub } exposing (downs)
 {-| TODO: 모듈레벨 설명 보충 -}
 
@@ -77,13 +86,12 @@ TODO: `List (MySub msg)` 설명 보충
 
 [Router]: http://package.elm-lang.org/packages/elm-lang/core/5.0.0/Platform#Router
 -}
-onEffects
-  : Platform.Router msg SelfMsg -> List (MySub msg) -> State msg -> Task Never (State msg)
-  --                                                                     ^^^^^
-  -- 이 모듈의 onEffects는 절대 실패하지 않는다. 그러므로 Task 타입의 첫번째
-  -- 타입변수에 Never 타입이 들어갔다.
+onEffects : Platform.Router msg SelfMsg -> List (MySub msg) -> State msg
+              -> Task Never (State msg)
+--                    ^^^^^
+-- 이 모듈의 onEffects는 절대 실패하지 않는다. 그러므로 Task 타입의 첫번째
+-- 타입변수에 Never 타입이 들어갔다.
 onEffects router newSubs oldState =
-  -- TODO: 이벤트핸들러 붙였다 떼었다 하면서 테스트하기
   let
     -- MySub 안에 들어있는 msg들을 모두 꺼내, List msg로 모은다
     newTaggers : List msg
@@ -93,34 +101,44 @@ onEffects router newSubs oldState =
     newWatcher : Process.Id -> Task Never (State msg)
     newWatcher pid = Task.succeed (Just (Watcher newTaggers pid))
 
-    -- TODO: 코드 순서 바꾸기
+    -- 기존에 프로세스가 있는 상황에서 tagger가 업데이트 되었을때 실행되는 함수.
+    -- 프로세스는 유지한채, Watcher 안의 tagger만 바꿔준다.
+    --
+    -- TODO: newTaggers가 []이면, 프로세스 죽이는 코드 만들기.
     updateProcess : Watcher msg -> Task Never (State msg)
     updateProcess {pid} = newWatcher pid
 
+    -- 기존에 프로세스가 없었던 상황에서 tagger가 업데이트되면 실행되는 함수. 새
+    -- 프로세스를 만들어주는 코드가 들어있다.
     createProcess : Task Never (State msg)
     createProcess =
       let
-        -- 별 의미없이 있는 함수. 원래는 onDocument 콜백의 결과로 주어지는
-        -- 키코드를 파싱하는데 쓰는 함수지만, 본 예제에선 키코드 값을 버리므로
+        -- 플레이스홀더 함수. 원래는 onDocument 콜백의 결과로 주어지는 키코드를
+        -- 어떻게 파싱할지 서술하는 함수지만, 본 예제에선 키코드 값을 버리므로
         -- 의미가 없다.
+        --
         -- Reference: http://package.elm-lang.org/packages/elm-lang/core/5.0.0/Json-Decode
         keyCode : Json.Decoder ()
         keyCode = Json.succeed ()
 
-        -- onDocument 함수에 넣을 콜백함수
+        -- onDocument 함수에 넣을 콜백함수. onDocument에서 이벤트가 발생하면,
+        -- SelfMsg를 발생시켜, `onSelfMsg`함수가 이벤트를 처리하도록 만든다.
         callBack : () -> Task Never ()
         callBack () = Platform.sendToSelf router MakeSelfMsg
 
         -- onDocument 호출로 생성한 프로미스
+        --
         -- Reference: http://package.elm-lang.org/packages/elm-lang/dom/1.1.1/Dom-LowLevel#onDocument
         promise : Task Never Never
         promise = Dom.onDocument "keydown" keyCode callBack
       in
-        Process.spawn promise
-        |> Task.andThen newWatcher
+        Process.spawn promise       -- 새 프로세스를 만들어서
+        |> Task.andThen newWatcher  -- 그 프로세스에 새 Watcher를 등록시킨다
   in
     case oldState of
+      -- 기존에 프로세스가 없었던경우, 새 프로세스를 만들어준다.
       Nothing   -> createProcess
+      -- 기존에 프로세스가 있었을경우, 프로세스 안의 Watcher만 업데이트해준다.
       Just left -> updateProcess left
 
 
@@ -130,20 +148,30 @@ Platform.sendToApp 함수를 호출하면 앱에 메시지가 간다. Platform.s
 함수를 호출하면 전송한 메시지가 이 모듈에게 돌아온다. 이때 그 자기자신에게 보낸
 메시지를 처리하는 함수가 바로 onSelfMsg 함수이다.
 
-TODO: 동작 설명하기 -}
-onSelfMsg : Platform.Router msg SelfMsg -> SelfMsg -> State msg -> Task Never (State msg)
+본 예제에서 onSelfMsg 함수는 `<body>` 태그에서 `keydown` 이벤트가 발생할때마다
+호출된다. `keydown` 이벤트가 발생했을때, 현재 Watcher가 있을경우 Watcher 안의
+이벤트핸들러들을 모두 호출시켜주며, Watcher가 없었다면 아무것도 하지 않는다. -}
+onSelfMsg : Platform.Router msg SelfMsg -> SelfMsg -> State msg
+              -> Task Never (State msg)
+--                    ^^^^^
+-- onEffects와 마찬가지로 이 모듈의 onSelfMsg 또한 절대 실패하지 않는다.
+-- 그러므로 Task 타입의 첫번째 타입변수에 Never 타입이 들어갔다.
 onSelfMsg router _ state =
   case state of
-    Nothing ->
-      Task.succeed state
-
-    Just {taggers} ->
+    -- 기존에 이벤트핸들러가 없었을경우, 아무것도 하지 않고 바로 리턴
+    Nothing -> Task.succeed state
+    -- 기존에 이벤트핸들러가 있었을경우, 핸들러들을 모두 호출한다.
+    Just watcher ->
       let
-        send tagger =
-          Platform.sendToApp router (tagger)
+        -- Watcher 안에 들어있는 모든 tagger들 꺼냄
+        taggerList = watcher.taggers
+
+        -- 배열 내의 모든 tagger들에 Platform.sendToApp 함수를 map 해서
+        -- Task들의 배열로 만듬
+        taskList = List.map (Platform.sendToApp router) taggerList
       in
-        Task.sequence (List.map send taggers)
-          |> Task.andThen (\_ -> Task.succeed state)
+        Task.sequence taskList                      -- taskList를 전부 수행한뒤
+        |> Task.andThen (\_ -> Task.succeed state)  -- state를 그대로 리턴
 
 
 {-| TODO: 뭐하는 함수인지 정확히 할아내기

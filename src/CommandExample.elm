38b7f99266f09559902f9eeffa7fb0da11858da1
Hyeon Kim <simnalamburt@gmail.com>
2016-12-09 14:46:24 +0900

    Explicity specity code license in source codes

--- src/CommandExample.elm
+++ src/CommandExample.elm
@@ -1,3 +1,13 @@
+{-
+Copyright 2016 Hyeon Kim
+
+Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+<LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+option. This file may not be copied, modified, or distributed
+except according to those terms.
+-}
+
 {-| MyCounter 시연용 더미 어플리케이션. -}
 import Html exposing (Html, div, button, text)
 import Html.Attributes exposing (style)
